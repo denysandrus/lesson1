7c6134d859477e39eed3ae2f08a63a0f196bb32c
“denysandrus” <denys.andrushchenko@gmail.com>
2017-10-30 08:43:03 +0200

    try2 L1
    rspec failed, rubocop OK, just another attempt

--- lesson1.rb
+++ lesson1.rb
@@ -1,11 +1,8 @@
 require 'date'
 class Lesson1
-  def sum(val = 0)
+  def sum(array = 0)
     # TODO
-    array val.to_s.split('')
-    total_sum = 0
-    array.each d9 |a|
-    total_sum += a.to_i
+    array.inject(0) { |sum, x| sum + x }
   end
 
   def age(birthday)
@@ -13,18 +10,17 @@ class Lesson1
     birth = Date.parse(birthday)
     days = (Date.today - birth).to_i
 
-    "I have been on this freaking planet for #{days / 365} years or #{days} days or #{days * 24} hours or \
-    #{days * 24 * 60}  minutes or #{days * 24 * 60 * 60} seconds"
+    puts "I am #{days / 365} years or #{days} days or #{days * 24} hours or
+    #{days * 24 * 60}  minutes or #{days * 24 * 60 * 60} seconds old."
     'Invalid Date Format'
   end
 
   def name
     # TODO
-    print "What is your name? "
+    print 'What is your name? '
     name = gets.chomp
-    print  "What is your last name? "
+    print  'What is your last name?'
     last_name = gets.chomp
-  puts "Hello #{[name, last_name].compact.join(' ')} Superman!"
-      #puts "Hello " + name + " " + last_name + " " + middle_name + " " + "Superman!"
+    puts "Hello #{[name, last_name].compact.join(' ')} Superman!"
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -2,62 +2,87 @@ class MyArray
   attr_accessor :array
 
   def initialize(arr = [])
+    # TODO
     @array = arr
   end
 
   def size
     # TODO
+    @array.size
   end
 
   def reverse
     # TODO
+    @array.reverse
   end
 
   def max
     # TODO
+    @array.max
   end
 
   def min
     # TODO
+    @array.min
   end
 
   def desc
     # TODO
+    @array.sort.reverse
   end
 
   def asc
     # TODO
+    @array.sort
   end
 
   def odd
     # TODO
+    @array.select(&:odd?)
   end
 
   def multiple_to_three
     # TODO
+    @array.select { |i| (i % 3).zero? }
   end
 
   def uniq
     # TODO
+    @array.uniq
   end
 
   def devide_on_ten
     # TODO
+    @array.collect { |x| x.to_f / 10 }
   end
 
   def chars
     # TODO
+    alphabet = (:a..:z).to_a
+    @array.map { |v| alphabet[v.to_i - 1] }
   end
 
   def switch
     # TODO
+    min = @array.min
+    max = @array.max
+    min_index = @array.index(min)
+    max_index = @array.index(max)
+    dup_array = @array.dup
+    dup_array[min_index] = max
+    dup_array[max_index] = min
+    dup_array
   end
 
   def before_min
     # TODO
+    min = @array.min
+    min_index = @array.index(min)
+    @array.take(min_index)
   end
 
   def three_smallest
     # TODO
+    @array.sort.take(3)
   end
 end
